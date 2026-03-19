f7704431f23e671b3d5886f67291679999bc2884
Matheus Benevenuto <matthewbene03@gmail.com>
2026-03-19 14:48:19 -0300

    Criando a estrutura de navegação por footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,28 @@
-function Footer(){
-    return(
-        <h1>Footer</h1>
+import { useLocation } from "react-router-dom";
+import { LuMapPin } from "react-icons/lu";
+import { GoHome } from "react-icons/go";
+import { MdOutlineCalendarMonth } from "react-icons/md";
+
+import { Container, LinkItem } from "./styled"
+
+function Footer() {
+    const location = useLocation();
+
+    return (
+        <Container>
+            <LinkItem to={"/"} $pagAtiva={location.pathname === "/"}>
+                <GoHome />
+                Início
+            </LinkItem>
+            <LinkItem to={"/calendario"} $pagAtiva={location.pathname === "/calendario"}>
+                <MdOutlineCalendarMonth />
+                Calendario
+            </LinkItem>
+            <LinkItem to={"/mapa"} $pagAtiva={location.pathname === "/mapa"}>
+                <LuMapPin />
+                Mapa
+            </LinkItem>
+        </Container >
     )
 }
 
---
+++ src/components/Footer/styled.tsx
@@ -0,0 +1,50 @@
+import styled from "styled-components";
+import * as colors from "../../config/colors"
+import { Link } from "react-router-dom";
+
+export const Container = styled.nav`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+    gap: 10px;
+    
+    padding: 10px 20px;
+    background-color: ${colors.PrimeiraCorClara};
+    
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    left: 0;
+`;
+
+interface Props {
+  $pagAtiva: boolean;
+}
+
+export const LinkItem = styled(Link)<Props>`
+    flex: 30%;
+
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 80%;
+        /* border: 2px solid red; */
+
+        text-decoration: none;
+        font-size: 18px;
+        font-weight: bold;
+        font-family: "Roboto", sans-serif;
+        color: ${colors.SegundaCorClara};
+
+        border-radius: 15px;
+        background-color: ${({ $pagAtiva }) => ($pagAtiva ? colors.PrimeiraCorClara: "none")};
+
+        cursor: pointer;
+        transition: all 300ms;
+    
+    &:hover{
+        background-color: ${colors.PrimeiraCorClara};
+    }
+`;
